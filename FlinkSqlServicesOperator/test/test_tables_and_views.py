e191d72f1c406d0de5889aed963c07f10f5fe198
Marcel <wagmarcel@web.de>
2022-02-06 20:51:39 +0100

    added lintig for table_and_views.py

--- FlinkSqlServicesOperator/test/test_tables_and_views.py
+++ FlinkSqlServicesOperator/test/test_tables_and_views.py
@@ -1,52 +1,74 @@
+"""
+Unittests for tables_and_views.py
+"""
 from unittest import TestCase
 import unittest
 from bunch import Bunch
-from mock import patch, MagicMock
-import kopf
-from kopf.testing import KopfRunner
+from mock import patch
 
 import tables_and_views as target
 
 
-"""
-Mock functions
-"""
-
-global_message = ''
+# Mock functions
+# --------------
+GLOBAL_MESSAGE = ''
 
 class Logger():
+    """
+    mock for logger
+    """
     def info(self, message):
-        pass
+        """
+        mock for info
+        """
 
     def debug(self, message):
-        pass
+        """
+        mock for debug
+        """
 
     def error(self, message):
-        pass
+        """
+        mock for error
+        """
 
+   # pylint: disable=global-statement,no-self-use
     def warning(self, message):
-        global global_message
-        global_message = message
-        pass
+        """
+        mock for warning
+        """
+        global GLOBAL_MESSAGE
+        GLOBAL_MESSAGE = message
 
-
+# pylint: disable=unused-argument
 def kopf_info(body, reason, message):
-    pass
+    """
+    mock for kopf_info
+    """
 
 
 
 class TestDdlCreation(TestCase):
+    """
+    tests for ddl creation
+    """
     create_kafka_ddl_used = False
     create_upsert_kafka_ddl_used = False
 
+   # pylint: disable=global-statement, no-self-use, no-self-argument
     def create_kafka_ddl(beamsqltable, logger):
+        """
+        mock for create_kafka_ddl
+        """
         TestDdlCreation.create_kafka_ddl_used = True
-        return
 
 
     @patch('tables_and_views.create_kafka_ddl',
         create_kafka_ddl)
     def test_create_ddl_from_beamsqltable(self):
+        """
+        test for create_ddl_from_beamsqltable
+        """
         TestDdlCreation.create_kafka_ddl_used = False
 
         beamsqltable = Bunch()
@@ -60,15 +82,20 @@ class TestDdlCreation(TestCase):
         target.create_ddl_from_beamsqltables(beamsqltable, Logger())
         self.assertEqual(TestDdlCreation.create_kafka_ddl_used, True)
 
-
+   # pylint: disable=global-statement, no-self-use, no-self-argument
     def create_upsert_kafka_ddl(beamsqltable, logger):
+        """
+        mock for create_upsert_kafka_ddl
+        """
         TestDdlCreation.create_upsert_kafka_ddl_used = True
-        return
 
 
     @patch('tables_and_views.create_upsert_kafka_ddl',
         create_upsert_kafka_ddl)
     def test_create_upsert_ddl_from_beamsqltable(self):
+        """
+        test for create_upsert_ddl_from_beamsqlable
+        """
         TestDdlCreation.create_upsert_kafka_ddl_used = False
 
         beamsqltable = Bunch()
@@ -83,8 +110,11 @@ class TestDdlCreation(TestCase):
         self.assertEqual(TestDdlCreation.create_upsert_kafka_ddl_used, True)
 
     def test_warning(self):
-        global global_message
-        global_message = ""
+        """
+        test warnings for create_ddl_from_beamsqltables without connector spec
+        """
+        global GLOBAL_MESSAGE
+        GLOBAL_MESSAGE = ""
 
         beamsqltable = Bunch()
         beamsqltable.metadata = Bunch()
@@ -95,13 +125,20 @@ class TestDdlCreation(TestCase):
         }
 
         target.create_ddl_from_beamsqltables(beamsqltable, Logger())
-        self.assertTrue(global_message.startswith("Beamsqltable name has not supported connector:"))
+        self.assertTrue(GLOBAL_MESSAGE.startswith("Beamsqltable name has not supported connector:"))
 
 
 class TestcreateKafkaDdl(TestCase):
+    """
+    tests for create_kafka_ddl
+    """
+       # pylint: disable=global-statement, no-self-argument
     def test_create_ddl_from_beamsqltable(self):
-        global global_message
-        global_message = ""
+        """
+        test successful kafka_ddl creation
+        """
+        global GLOBAL_MESSAGE
+        GLOBAL_MESSAGE = ""
         beamsqltable = Bunch()
         beamsqltable.metadata = Bunch()
         beamsqltable.metadata.name = "name"
@@ -126,11 +163,15 @@ class TestcreateKafkaDdl(TestCase):
 
         result = target.create_kafka_ddl(beamsqltable, Logger())
         self.assertEqual(result, "CREATE TABLE `name` (`field1` field1,`field2` field2) WITH "\
-            "('connector' = 'kafka','format' = 'json', 'topic' = 'topic','properties.bootstrap.servers' = 'bootstrap.servers');")
+            "('connector' = 'kafka','format' = 'json', 'topic' = 'topic',"\
+            "'properties.bootstrap.servers' = 'bootstrap.servers');")
 
     def test_create_ddl_from_beamsqltable_noformat(self):
-        global global_message
-        global_message = ""
+        """
+        test create_ddl_from_eamsqltable withtout format spec
+        """
+        global GLOBAL_MESSAGE
+        GLOBAL_MESSAGE = ""
         beamsqltable = Bunch()
         beamsqltable.metadata = Bunch()
         beamsqltable.metadata.name = "name"
@@ -155,9 +196,12 @@ class TestcreateKafkaDdl(TestCase):
         result = target.create_kafka_ddl(beamsqltable, Logger())
         self.assertEqual(result, None)
 
-    def test_create_ddl_from_beamsqltable_noValue(self):
-        global global_message
-        global_message = ""
+    def test_create_ddl_from_beamsqltable_no_value(self):
+        """
+        test create_ddl_from_beamsqltable with no value in spec
+        """
+        global GLOBAL_MESSAGE
+        GLOBAL_MESSAGE = ""
         beamsqltable = Bunch()
         beamsqltable.metadata = Bunch()
         beamsqltable.metadata.name = "name"
@@ -182,12 +226,16 @@ class TestcreateKafkaDdl(TestCase):
 
         result = target.create_kafka_ddl(beamsqltable, Logger())
         self.assertEqual(result, "CREATE TABLE `name` (`field1` field1,`field2` field2) WITH "\
-            "('connector' = 'kafka','format' = 'json', 'topic' = 'topic','properties.bootstrap.servers' = 'bootstrap.servers');")
+            "('connector' = 'kafka','format' = 'json', 'topic' = 'topic'"\
+            ",'properties.bootstrap.servers' = 'bootstrap.servers');")
 
 
     def test_create_ddl_from_beamsqltable_notopic(self):
-        global global_message
-        global_message = ""
+        """
+        test create_ddl_from_beamsqltable with no topic in spec
+        """
+        global GLOBAL_MESSAGE
+        GLOBAL_MESSAGE = ""
         beamsqltable = Bunch()
         beamsqltable.metadata = Bunch()
         beamsqltable.metadata.name = "name"
@@ -214,8 +262,11 @@ class TestcreateKafkaDdl(TestCase):
 
 
     def test_create_ddl_from_beamsqltable_noname(self):
-        global global_message
-        global_message = ""
+        """
+        test create_ddl_from_beamsqltable with no name in spec
+        """
+        global GLOBAL_MESSAGE
+        GLOBAL_MESSAGE = ""
         beamsqltable = Bunch()
         beamsqltable.metadata = Bunch()
         beamsqltable.metadata.name = "name"
@@ -239,14 +290,18 @@ class TestcreateKafkaDdl(TestCase):
 
         result = target.create_kafka_ddl(beamsqltable, Logger())
         self.assertEqual(result, "CREATE TABLE `name` (`field1` field1,`field2` field2) WITH "\
-            "('connector' = 'kafka','format' = 'json', 'topic' = 'topic','properties.bootstrap.servers' = 'bootstrap.servers');")
+            "('connector' = 'kafka','format' = 'json', 'topic' = 'topic'"\
+            ",'properties.bootstrap.servers' = 'bootstrap.servers');")
 
 
 
 
     def test_create_ddl_from_beamsqltable_nobootstrap(self):
-        global global_message
-        global_message = ""
+        """
+        test creae_ddl_from_beamsqltable with no bootstrap in spec
+        """
+        global GLOBAL_MESSAGE
+        GLOBAL_MESSAGE = ""
         beamsqltable = Bunch()
         beamsqltable.metadata = Bunch()
         beamsqltable.metadata.name = "name"
@@ -273,8 +328,11 @@ class TestcreateKafkaDdl(TestCase):
 
 
     def test_create_ddl_from_beamsqltable_nokafka(self):
-        global global_message
-        global_message = ""
+        """
+        test create_ddl_from_beamsqltable with no kafka in spec
+        """
+        global GLOBAL_MESSAGE
+        GLOBAL_MESSAGE = ""
         beamsqltable = Bunch()
         beamsqltable.metadata = Bunch()
         beamsqltable.metadata.name = "name"
@@ -296,8 +354,11 @@ class TestcreateKafkaDdl(TestCase):
 
 
     def test_create_ddl_from_beamsqltable_novalue(self):
-        global global_message
-        global_message = ""
+        """
+        test create_ddl_from_beamsqltable wiht no value in spec
+        """
+        global GLOBAL_MESSAGE
+        GLOBAL_MESSAGE = ""
         beamsqltable = Bunch()
         beamsqltable.metadata = Bunch()
         beamsqltable.metadata.name = "name"
@@ -315,8 +376,11 @@ class TestcreateKafkaDdl(TestCase):
         self.assertEqual(result, None)
 
     def test_create_upsert_ddl_from_beamsqltable(self):
-        global global_message
-        global_message = ""
+        """
+        test create_upsert_ddl_from_beamsqltable
+        """
+        global GLOBAL_MESSAGE
+        GLOBAL_MESSAGE = ""
         beamsqltable = Bunch()
         beamsqltable.metadata = Bunch()
         beamsqltable.metadata.name = "name"
@@ -342,14 +406,19 @@ class TestcreateKafkaDdl(TestCase):
         }
 
         result = target.create_upsert_kafka_ddl(beamsqltable, Logger())
-        self.assertEqual(result, "CREATE TABLE `name` (`field1` field1,`field2` field2, PRIMARY KEY (primaryKey) NOT ENFORCED) WITH "\
-            "('connector' = 'upsert-kafka','value.format' = 'json', 'topic' = 'topic', 'key.format' = 'json',"\
+        self.assertEqual(result, "CREATE TABLE `name` (`field1` field1,`field2` field2,"\
+            " PRIMARY KEY (primaryKey) NOT ENFORCED) WITH "\
+            "('connector' = 'upsert-kafka','value.format' = 'json', 'topic' = 'topic',"\
+            " 'key.format' = 'json',"\
             "'properties.bootstrap.servers' = 'bootstrap.servers');")
 
 
     def test_create_upsert_ddl_from_beamsqltable_novalue(self):
-        global global_message
-        global_message = ""
+        """
+        test create_upsert_ddl_from_beamsqltable with no value in spec
+        """
+        global GLOBAL_MESSAGE
+        GLOBAL_MESSAGE = ""
         beamsqltable = Bunch()
         beamsqltable.metadata = Bunch()
         beamsqltable.metadata.name = "name"
@@ -375,8 +444,11 @@ class TestcreateKafkaDdl(TestCase):
         self.assertEqual(result, None)
 
     def test_create_upsert_ddl_from_beamsqltable_noname(self):
-        global global_message
-        global_message = ""
+        """
+        test create_upsert_ddl_from_beamsqltable with no name in spec
+        """
+        global GLOBAL_MESSAGE
+        GLOBAL_MESSAGE = ""
         beamsqltable = Bunch()
         beamsqltable.metadata = Bunch()
         beamsqltable.metadata.name = "name"
@@ -401,13 +473,18 @@ class TestcreateKafkaDdl(TestCase):
         }
 
         result = target.create_upsert_kafka_ddl(beamsqltable, Logger())
-        self.assertEqual(result, "CREATE TABLE `name` (`field1` field1,`field2` field2, PRIMARY KEY (primaryKey) NOT ENFORCED) WITH "\
-            "('connector' = 'upsert-kafka','value.format' = 'json', 'topic' = 'topic', 'key.format' = 'json',"\
+        self.assertEqual(result, "CREATE TABLE `name` (`field1` field1,`field2` field2,"\
+            " PRIMARY KEY (primaryKey) NOT ENFORCED) WITH "\
+            "('connector' = 'upsert-kafka','value.format' = 'json', 'topic' = 'topic',"\
+            " 'key.format' = 'json',"\
             "'properties.bootstrap.servers' = 'bootstrap.servers');")
 
     def test_create_upsert_ddl_from_beamsqltable_fail(self):
-        global global_message
-        global_message = ""
+        """
+        test create_upsert_ddl_from_beamsqltable
+        """
+        global GLOBAL_MESSAGE
+        GLOBAL_MESSAGE = ""
         beamsqltable = Bunch()
         beamsqltable.metadata = Bunch()
         beamsqltable.metadata.name = "name"
@@ -435,6 +512,9 @@ class TestcreateKafkaDdl(TestCase):
         self.assertEqual(result, None)
 
     def test_create_create_view(self):
+        """
+        test create_view
+        """
         beamsqlview = Bunch()
         beamsqlview.metadata = Bunch()
         beamsqlview.metadata.name = "name"
