330092203d6ec9810e0eba505b78100f20fb480c
Marcel <wagmarcel@web.de>
2022-02-06 18:50:04 +0100

    update linting for test_flink_util

--- FlinkSqlServicesOperator/test/test_flink_util.py
+++ FlinkSqlServicesOperator/test/test_flink_util.py
@@ -1,51 +1,76 @@
+"""
+Unittest for flink_util.yp
+"""
 from unittest import TestCase
 import unittest
 from bunch import Bunch
-from mock import patch, MagicMock
-import kopf
-from kopf.testing import KopfRunner
-
-import flink_util as target
+from mock import patch
 import requests
 
+import flink_util as target
 
-"""
-Mock functions
-"""
 
-global_message = ''
+# Mock functions
+# --------------
+
+GLOBAL_MESSAGE = ''
 
 class Logger():
+    """
+    mock for logger
+    """
     def info(self, message):
-        pass
+        """
+        mock for info
+        """
 
     def debug(self, message):
-        pass
+        """
+        mock for debug
+        """
 
     def error(self, message):
-        pass
+        """
+        mock for error
+        """
 
+    # pylint: disable=global-statement,no-self-use
     def warning(self, message):
-        global global_message
-        global_message = message
-        pass
+        """
+        mock for warning
+        """
+        global GLOBAL_MESSAGE
+        GLOBAL_MESSAGE = message
 
-
+# pylint: disable=unused-argument
 def kopf_info(body, reason, message):
-    pass
-
+    """
+    mock for kopf_info
+    """
 
 def raise_for_status_success():
+    """
+    mock for successful status
+    """
     return
 
 
 def raise_for_status_fail():
+    """
+    mock for unsuccessful status get
+    """
     raise requests.exceptions.HTTPError
 
-def getJson():
+def get_json():
+    """
+    retrieve empty JSON
+    """
     return "{}"
 
-def getJsonStatus():
+def get_json_status():
+    """
+    retrieve JSON with status
+    """
     return {'status': {
         'id': 'id'
         },
@@ -54,81 +79,119 @@ def getJsonStatus():
         }
     }
 
-def getJsonRequestId():
+def get_json_request_id():
+    """
+    result of get request
+    """
     requestid = Bunch()
     requestid['request-id'] = 'request-id'
     return requestid
 
 def request_get_successful(url):
+    """
+    get response from get request
+    """
     response = Bunch()
     response.raise_for_status = raise_for_status_success
-    response.json = getJson
+    response.json = get_json
     response.status_code = 200
     return response
 
 def request_get_failed(url):
+    """
+    get request failed
+    """
     response = Bunch()
     response.raise_for_status = raise_for_status_fail
-    response.json = getJson
+    response.json = get_json
     response.status_code = 404
     return response
 
 def request_get_successful_job_status(url):
+    """
+    get job status
+    """
     response = Bunch()
     response.raise_for_status = raise_for_status_success
-    response.json = getJsonStatus
+    response.json = get_json_status
     response.status_code = 200
     return response
 
 def request_post_successful(url, json):
+    """
+    post request
+    """
     response = Bunch()
     response.raise_for_status = raise_for_status_success
-    response.json = getJsonRequestId
+    response.json = get_json_request_id
     response.status_code = 202
     return response
 
 def request_patch_successful(url):
+    """
+    send patch request
+    """
     response = Bunch()
     response.status_code = 202
     return response
 
 class TestJobStatus(TestCase):
-
-
+    """
+    Test class for job stati
+    """
     @patch('requests.get', request_get_successful)
     def test_job_status(self):
+        """
+        test job_status
+        """
         job_id ="job_id"
         response = target.get_job_status(Logger(), job_id)
         self.assertEqual(response, "{}")
-    
+
     @patch('requests.get', request_get_successful_job_status)
     def test_get_savepoint_state(self):
+        """
+        test for get_savepoint_state
+        """
         job_id ="job_id"
         savepoint_dir = "savepoint_dir"
         response = target.get_savepoint_state(Logger(), job_id, savepoint_dir)
         self.assertEqual(response, {'status': 'id', 'location': 'location'})
 
+    # pylint: disable=global-statement,no-self-use
     @patch('requests.patch', request_patch_successful)
     def test_cancel_job(self):
+        """
+        test for cancel_job
+        """
         job_id ="job_id"
         target.cancel_job(Logger(), job_id)
 
     @patch('requests.post', request_post_successful)
     def test_stop_job(self):
+        """
+        test for stop_job
+        """
         job_id ="job_id"
         savepoint_dir = "savepoint_dir"
         response = target.stop_job(Logger(), job_id, savepoint_dir)
         self.assertEqual(response, 'request-id')
 
-    @patch('requests.get', request_get_successful_job_status)
-    def test_get_savepoint_state(self):
-        job_id ="job_id"
-        savepoint_dir = "savepoint_dir"
-        response = target.get_savepoint_state(Logger(), job_id, savepoint_dir)
-        self.assertEqual(response, {'status': 'id', 'location': 'location'})
+    #@patch('requests.get', request_get_successful_job_status)
+    #def test_get_savepoint_state(self):
+    #    """
+    #    test for get_savepoint
+    #    """
+    #    job_id ="job_id"
+    #    savepoint_dir = "savepoint_dir"
+    #    response = target.get_savepoint_state(Logger(), job_id, savepoint_dir)
+    #    self.assertEqual(response, {'status': 'id', 'location': 'location'})
 
     @patch('requests.get', request_get_failed)
     def test_get_savepoint_state_fail(self):
+        """
+        test for get_savepoint_state with fail
+        """
         job_id ="job_id"
         savepoint_dir = "savepoint_dir"
         response = target.get_savepoint_state(Logger(), job_id, savepoint_dir)
--- FlinkSqlServicesOperator/test/test_util.py
+++ FlinkSqlServicesOperator/test/test_util.py
@@ -1,57 +1,86 @@
+"""
+Unittest for util.py
+"""
+
 from unittest import TestCase
 import unittest
-from urllib import response
 from bunch import Bunch
-from mock import patch, MagicMock
-import kopf
-from kopf.testing import KopfRunner
+from mock import patch
 
 import util as target
-import requests
 
 
-"""
-Mock functions
-"""
+# Mock functions
+# --------------
 
 get_environ_ENV = {
     "conf": '{ "a": "b"}'
 }
 
+
 def oisp_token():
+    """
+    return mock token
+    """
     value = Bunch()
     value.value = "token"
     return value
 
+# pylint: disable=unused-argument
 def auth_pass(user, password):
-    pass
+    """
+    Mock successful auth
+    """
+
 
 def oisp_pass(url):
+    """
+    Mock successfull oisp auth
+    """
     client = Bunch()
     client.auth = auth_pass
     client.get_user_token = oisp_token
     return client
 
-def base64Enc(value):
+
+def base64_enc(value):
+    """
+    Mock base64
+    """
     return f"base64+{value}".encode('utf-8')
 
+
 class TestUtils(TestCase):
-    
+    """
+    Test class for all util functions
+    """
     @patch('os.environ', get_environ_ENV)
     def test_load_config(self):
+        """
+        test load config
+        """
         response = target.load_config_from_env("conf")
         self.assertEqual(response, {"a": "b"})
 
     @patch('oisp.Client', oisp_pass)
     def test_get_tokens(self):
-        response = target.get_tokens([{"user" : "username", "password": "password"}])
+        """
+        test get tokens
+        """
+        response = target.get_tokens(
+            [{"user": "username", "password": "password"}])
         self.assertEqual(response, {"username": "token"})
 
-    #@patch('config', {})
-    @patch('base64.b64encode', base64Enc)
+    # @patch('config', {})
+    @patch('base64.b64encode', base64_enc)
     def test_format_template(self):
-        response = target.format_template("string{tokens}", tokens="tokensss", encode='base64')
+        """
+        test format_template
+        """
+        response = target.format_template(
+            "string{tokens}", tokens="tokensss", encode='base64')
         self.assertEqual("base64+b'stringtokensss'", response)
+
 
 if __name__ == '__main__':
     unittest.main()
