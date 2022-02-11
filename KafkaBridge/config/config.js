211d4db6f64182ea85fa798bd8e173bbef2e44ce
Marcel <wagmarcel@web.de>
2022-02-11 11:20:41 +0100

    first draft of alerta bridge

---
+++ KafkaBridge/config/config.js
@@ -0,0 +1,39 @@
+
+module.exports ={
+	"kafka": {
+		"brokers": ["my-cluster-kafka-bootstrap:9092"],
+	},
+	"alerta": {
+		"topic": "iff.alerts",
+		"hostname": "alerta",
+		"port": "8080",
+		"protocol": "http:",
+		"tokenRefreshInterval": 60,
+		"accessKey": "t9ODS70LWNqEEo_0f2lDN7bnTAfiW0ucciAthrHj"
+	},
+	"logger": {
+		"loglevel": "debug"
+	},
+	"keycloak": {
+		"ngsildUpdatesAuthService": {
+			"port": 80,
+			"auth-server-url":  "http://keycloak.local/auth",
+			"realm": "org",
+			"clientId": "ngsildUpdates",
+			"resource": "ngsildUpdates",
+			"secret": "g5mazZJStwUpJGyochsHQC3kS7gnwycA",
+			"bearer-only": true,
+			"verify-token-audience": false,
+			"ssl-required": false
+		}
+	},
+	"ngsildUpdates": {
+		"topic": "iff.ngsildUpdates",
+		"refreshIntervalInSeconds": 60
+	},        
+	"ngsildServer": {
+		"host": "ngsild.local",
+		"port": 80
+	}
+
+}
