c065a58314f60c2b023648d2ced620b3978b7d05
Marcel <wagmarcel@web.de>
2022-02-20 00:53:28 +0100

    test for lib/debeziumBridge.js

---
+++ KafkaBridge/test/testLibDebeziumBridgte.js
@@ -0,0 +1,79 @@
+/**
+* Copyright (c) 2022 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+'use strict';
+
+const { assert } = require('chai');
+const chai = require('chai');
+global.should = chai.should();
+
+const rewire = require('rewire');
+const toTest = rewire('../lib/debeziumBridge.js');
+
+const logger = {
+    debug: function () {},
+    error: function () {}
+  };
+  
+describe('Test diff Attributes', function () {
+    it('Should return no updates and no deletions', async function () {
+        var config = {}
+        var Logger = function() {
+            return logger;
+        }
+        var beforeAttrs = {
+            "attr1": [{
+                "value": "value",
+                "index": 0
+            }]
+        }
+        var afterAttrs = {
+            "attr1": [{
+                "value": "value",
+                "index": 0
+            }]
+        }
+        var revert = toTest.__set__("Logger", Logger); 
+        var debeziumBridge = new toTest(config);
+        var result = debeziumBridge.diffAttributes(beforeAttrs, afterAttrs);
+        assert.deepEqual(result.updatedAttrs, {});
+        assert.deepEqual(result.deletedAttrs, {});
+        revert();
+    });
+    it('Should update value but no deletions', async function () {
+        var config = {}
+        var Logger = function() {
+            return logger;
+        }
+        var beforeAttrs = {
+            "attr1": [{
+                "value": "value",
+                "index": 0
+            }]
+        }
+        var afterAttrs = {
+            "attr1": [{
+                "value": "value2",
+                "index": 0
+            }]
+        }
+        var revert = toTest.__set__("Logger", Logger); 
+        var debeziumBridge = new toTest(config);
+        var result = debeziumBridge.diffAttributes(beforeAttrs, afterAttrs);
+        assert.deepEqual(result.updatedAttrs, afterAttrs);
+        assert.deepEqual(result.deletedAttrs, {});
+        revert();
+    });
+});  
