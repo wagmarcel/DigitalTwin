36d39017cd510aa191ed5cb76e717ef265c03bce
Marcel <wagmarcel@web.de>
2022-02-27 15:11:59 +0100

    and the tests

--- KafkaBridge/test/testLibNgsildUpdates.js
+++ KafkaBridge/test/testLibNgsildUpdates.js
@@ -20,153 +20,149 @@ const chai = require('chai');
 global.should = chai.should();
 
 const rewire = require('rewire');
-const { rest } = require('underscore');
-const toTest = rewire('../lib/ngsildUpdates.js');
+const ToTest = rewire('../lib/ngsildUpdates.js');
 
 const logger = {
-    debug: function () {},
-    error: function () {}
-  };
-  
+  debug: function () {},
+  error: function () {}
+};
 
 describe('Test libNgsildUpdates', function () {
-    it('Should post body with correct path and token for nonOverwrite update', async function () {
+  it('Should post body with correct path and token for nonOverwrite update', async function () {
+    const config = {
+      ngsildUpdates: {
+        clientSecretVariable: 'CLIENT_SECRET',
+        refreshIntervalInSeconds: 200
+      },
+      keycloak: {
+        ngsildUpdatesAuthService: {
 
-        var config = {
-            ngsildUpdates: {
-                clientSecretVariable: "CLIENT_SECRET",
-                refreshIntervalInSeconds: 200
-            },
-            keycloak: {
-                ngsildUpdatesAuthService: {
+        }
+      }
+    };
+    const Logger = function () {
+      return logger;
+    };
+    const process = {
+      env: {
+        CLIENT_SECRET: 'client_secret'
+      }
+    };
+    const body = {
+      op: 'update',
+      entity: 'entity',
+      id: 'id',
+      overwrite: false
+    };
+    const expHeaders = {
+      Authorization: 'Bearer token'
+    };
+    const Ngsild = function () {
+      return {
+        updateProperties: function (id, entity, noOverwrite, { headers }) {
+          id.should.equal('id');
+          entity.should.equal('entity');
+          noOverwrite.should.equal(true);
+          assert.deepEqual(headers, expHeaders);
+          return {
+            statusCode: 204
+          };
+        },
+        replaceEntities: function () {
+        }
+      };
+    };
+    const setInterval = function (fun, interv) {
+    };
+    const Keycloak = function () {
+      return {
+        grantManager: {
+          obtainFromClientCredentials: async function () {
+            return new Promise(function (resolve, reject) {
+              resolve({
+                access_token: {
+                  token: 'token'
+                }
+              });
+            });
+          }
+        }
+      };
+    };
+    const revert = ToTest.__set__('Logger', Logger);
+    ToTest.__set__('process', process);
+    ToTest.__set__('NgsiLd', Ngsild);
+    ToTest.__set__('setInterval', setInterval);
+    ToTest.__set__('Keycloak', Keycloak);
+    const ngsildUpdates = new ToTest(config);
+    await ngsildUpdates.ngsildUpdates(body);
+    revert();
+  });
+  it('Should post body with correct path and token for nonOverwrite upsert', async function () {
+    const config = {
+      ngsildUpdates: {
+        clientSecretVariable: 'CLIENT_SECRET',
+        refreshIntervalInSeconds: 200
+      },
+      keycloak: {
+        ngsildUpdatesAuthService: {
 
+        }
+      }
+    };
+    const Logger = function () {
+      return logger;
+    };
+    const process = {
+      env: {
+        CLIENT_SECRET: 'client_secret'
+      }
+    };
+    const body = {
+      op: 'upsert',
+      entity: 'entity',
+      id: 'id',
+      overwrite: false
+    };
+    const expHeaders = {
+      Authorization: 'Bearer token'
+    };
+    const Ngsild = function () {
+      return {
+        replaceEntities: function ([entity], noOverwrite, { headers }) {
+          entity.should.equal('entity');
+          noOverwrite.should.equal(true);
+          assert.deepEqual(headers, expHeaders);
+          return {
+            statusCode: 204
+          };
+        }
+      };
+    };
+    const setInterval = function (fun, interv) {
+    };
+    const Keycloak = function () {
+      return {
+        grantManager: {
+          obtainFromClientCredentials: async function () {
+            return new Promise(function (resolve, reject) {
+              resolve({
+                access_token: {
+                  token: 'token'
                 }
-            }
+              });
+            });
+          }
         }
-        var Logger = function() {
-            return logger;
-        }
-        var process = {
-            env: {
-                "CLIENT_SECRET": "client_secret"
-            }
-        }
-        var body = {
-            "op":  "update",
-            "entity": "entity",
-            "id": "id",
-            "overwrite": false
-        }
-        var expHeaders = {
-            "Authorization": "Bearer token"
-        }
-        const Ngsild = function(){
-            return {
-                updateProperties: function(id, entity, noOverwrite, {headers}) {
-                    id.should.equal("id");
-                    entity.should.equal("entity");
-                    noOverwrite.should.equal(true);
-                    assert.deepEqual(headers, expHeaders);
-                    return {
-                        statusCode: 204
-                    }
-                },
-                replaceEntities: function(){
-                }
-            }
-        }
-        const setInterval = function(fun, interv){
-        }
-        const Keycloak = function(){
-            return {
-                grantManager: {
-                    obtainFromClientCredentials: async function() {
-                        return new Promise(function(resolve, reject){
-                            resolve({
-                                access_token: {
-                                    token: "token"
-                                }
-                            })
-                        })
-                    }
-                }
-            }
-        }
-        var revert = toTest.__set__("Logger", Logger);
-        toTest.__set__("process", process);
-        toTest.__set__("NgsiLd", Ngsild);
-        toTest.__set__("setInterval", setInterval);
-        toTest.__set__("Keycloak", Keycloak);
-        var ngsildUpdates = new toTest(config);
-        var result = await ngsildUpdates.ngsildUpdates(body);
-        revert();
-    });
-    it('Should post body with correct path and token for nonOverwrite upsert', async function () {
-
-        var config = {
-            ngsildUpdates: {
-                clientSecretVariable: "CLIENT_SECRET",
-                refreshIntervalInSeconds: 200
-            },
-            keycloak: {
-                ngsildUpdatesAuthService: {
-
-                }
-            }
-        }
-        var Logger = function() {
-            return logger;
-        }
-        var process = {
-            env: {
-                "CLIENT_SECRET": "client_secret"
-            }
-        }
-        var body = {
-            "op":  "upsert",
-            "entity": "entity",
-            "id": "id",
-            "overwrite": false
-        }
-        var expHeaders = {
-            "Authorization": "Bearer token"
-        }
-        const Ngsild = function(){
-            return {
-                replaceEntities: function([entity], noOverwrite, {headers}) {
-                    entity.should.equal("entity");
-                    noOverwrite.should.equal(true);
-                    assert.deepEqual(headers, expHeaders);
-                    return {
-                        statusCode: 204
-                    }
-                }
-            }
-        }
-        const setInterval = function(fun, interv){
-        }
-        const Keycloak = function(){
-            return {
-                grantManager: {
-                    obtainFromClientCredentials: async function() {
-                        return new Promise(function(resolve, reject){
-                            resolve({
-                                access_token: {
-                                    token: "token"
-                                }
-                            })
-                        })
-                    }
-                }
-            }
-        }
-        var revert = toTest.__set__("Logger", Logger);
-        toTest.__set__("process", process);
-        toTest.__set__("NgsiLd", Ngsild);
-        toTest.__set__("setInterval", setInterval);
-        toTest.__set__("Keycloak", Keycloak);
-        var ngsildUpdates = new toTest(config);
-        var result = await ngsildUpdates.ngsildUpdates(body);
-        revert();
-    });
+      };
+    };
+    const revert = ToTest.__set__('Logger', Logger);
+    ToTest.__set__('process', process);
+    ToTest.__set__('NgsiLd', Ngsild);
+    ToTest.__set__('setInterval', setInterval);
+    ToTest.__set__('Keycloak', Keycloak);
+    const ngsildUpdates = new ToTest(config);
+    await ngsildUpdates.ngsildUpdates(body);
+    revert();
+  });
 });
