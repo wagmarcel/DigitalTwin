b9c0797641f4f3400aae48780d35d6bd52e3e478
Marcel <wagmarcel@web.de>
2022-10-17 14:23:51 +0200

    added test

--- semantic-model/shacl2flink/tests/test_create_sql_check_from_shacl.py
+++ semantic-model/shacl2flink/tests/test_create_sql_check_from_shacl.py
@@ -4,13 +4,16 @@ import os
 import create_sql_checks_from_shacl
 
 
+@patch('create_sql_checks_from_shacl.translate_properties')
 @patch('create_sql_checks_from_shacl.ruamel.yaml')
 @patch('create_sql_checks_from_shacl.utils')
-def test_main(mock_utils, mock_yaml, tmp_path):
+def test_main(mock_utils, mock_yaml, mock_translate_properties, tmp_path):
     def __add__(self, other):
         return self
 
     mock_utils.create_statementset.return_value = 'create_statementsets'
+
+    mock_translate_properties.return_value = 'sqlite', ('statementsets', ['tables'], ['views'])
 
     create_sql_checks_from_shacl.main('kms/shacl.ttl', 'kms/knowledge.ttl',
                                       tmp_path)
