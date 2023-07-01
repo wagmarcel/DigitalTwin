650730f87820107cdad8ff5e1f15d37e4f69b460
marcel <wagmarcel@web.de>
2023-07-01 23:29:15 +0200

    first version of create_udfs.py

---
+++ semantic-model/shacl2flink/create_udfs.py
@@ -0,0 +1,60 @@
+#
+# Copyright (c) 2023 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import argparse
+import os.path
+import sys
+import math
+import hashlib
+import owlrl
+import ruamel.yaml
+import rdflib
+from lib import utils
+from lib import configs
+import glob
+
+
+udfdir = 'udf/*/flink_*.py'
+udfcrdname = 'udf.yaml'
+
+def main(output_folder='output'):
+    yaml = ruamel.yaml.YAML()
+
+    utils.create_output_folder(output_folder)
+    udfs_full =  glob.glob(udfdir) 
+    
+    with open(os.path.join(output_folder, udfcrdname), 'w') as f:
+        for file in udfs_full:
+            with open(file) as fi:
+                txt = fi.read()
+            file_base = os.path.basename(os.path.splitext(file)[0])
+            _, file_name, file_ver = file_base.split('_')
+            crd = {}
+            crd['apiVersion'] = 'industry-fusion.com/v1alpha1'
+            crd['kind'] = 'flinkpythonudf'
+            metadata = {}
+            metadata['name'] = file_name
+            spec = {}
+            spec['filename'] = file_name
+            spec['version'] = file_ver
+            spec['class'] = txt
+            crd['metadata'] = metadata
+            crd['spec'] = spec
+            print('---', file=f)
+            yaml.dump(crd, f)
+
+if __name__ == '__main__':
+    main()
