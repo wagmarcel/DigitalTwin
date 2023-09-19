c1f2da0b9a0e408d7fba4a2a959a9bb8fbe58c4b
marcel <wagmarcel@web.de>
2023-09-19 23:00:21 +0200

    first version

--- semantic-model/kms/examples/evaluate.sh
+++ semantic-model/kms/examples/evaluate.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 TOOLPREFIX=../../shacl2flink
-OUTPUTDIR=files
+OUTPUTDIR=output
 SHACL=shacl.ttl
 QUOTE="'"
 PYTHON=python3
 KNOWLEDGE="${OUTPUTDIR}/knowledge.ttl"
+SQLITEDB=${OUTPUTDIR}/database.db
+SQLITE3=sqlite3
+sqlite_files="${OUTPUTDIR}/core.sqlite ${OUTPUTDIR}/ngsild.sqlite ${OUTPUTDIR}/rdf.sqlite ${OUTPUTDIR}/ngsild-models.sqlite ${OUTPUTDIR}/shacl-validation.sqlite"
 
 evaluate() {
     MODEL=$1
@@ -17,17 +20,28 @@ evaluate() {
     ${PYTHON} ${TOOLPREFIX}/create_sql_checks_from_shacl.py ${OUTPUTDIR}/${SHACL} ${KNOWLEDGE}
 }
 
-rm -rf ${OUTPUTDIR}
-mkdir -p ${OUTPUTDIR}
+get_results() {
+    echo Test with sqlite
+    cat ${sqlite_files} | ${SQLITE3} ${SQLITEDB}
+    echo "Alerts"
+    echo ------------------------------------------------
+    echo 'select * from alerts_bulk;'| sqlite3 ${SQLITEDB}
 
-touch ${OUTPUTDIR}/knowledge.ttl
-eval $(echo "yq ${QUOTE}.\"${SHACL}\"${QUOTE}" example1.yaml) > ${OUTPUTDIR}/${SHACL}
-fields=$(yq eval 'keys | .[] ' example1.yaml)
-for field in $fields; do
-    echo $field
-    if [[ "$field" =~ object* ]]; then
-        echo $field hello
-        eval $(echo "yq ${QUOTE}.\"${field}\"${QUOTE}" example1.yaml) > ${OUTPUTDIR}/${field}
-        evaluate "${OUTPUTDIR}/$field"
-    fi
+}
+
+for file in example*.yaml; do
+    rm -rf ${OUTPUTDIR}
+    mkdir -p ${OUTPUTDIR}
+
+    touch ${OUTPUTDIR}/knowledge.ttl
+    eval $(echo "yq ${QUOTE}.\"${SHACL}\"${QUOTE}" ${file}) > ${OUTPUTDIR}/${SHACL}
+    fields=$(yq eval 'keys | .[] ' example1.yaml)
+    for field in $fields; do
+        if [[ "$field" =~ object* ]]; then
+            echo Processing $field
+            eval $(echo "yq ${QUOTE}.\"${field}\"${QUOTE}" example1.yaml) > ${OUTPUTDIR}/${field}
+            evaluate "${OUTPUTDIR}/$field"
+            get_results
+        fi
+    done
 done
