8492c2a0b80d348cdb2d60d860bbfee8003a96cd
marcel <wagmarcel@web.de>
2024-07-31 11:49:27 +0200

    Added test for DI

--- semantic-model/opcua/tests/testnodeset2owl.bash
+++ semantic-model/opcua/tests/testnodeset2owl.bash
@@ -18,8 +18,10 @@ export LASERSYSTEMS_EXAMPLE_NODESET=https://raw.githubusercontent.com/OPCFoundat
 export BASE_ONTOLOGY=https://industryfusion.github.io/contexts/staging/ontology/v0.1/base.ttl
 export PACKML_NODESET=https://raw.githubusercontent.com/OPCFoundation/UA-Nodeset/UA-1.05.03-2023-12-15/PackML/Opc.Ua.PackML.NodeSet2.xml
 RESULT=result.ttl
+CLEANED=cleaned.ttl
 DEBUG=true
 TESTNODESETS=(test_object_types.NodeSet2 test_objects.NodeSet2)
+CLEANGRAPH=cleangraph.py
 for nodeset in "${TESTNODESETS[@]}"; do
     echo test $nodeset
     if [ "$DEBUG"="true" ]; then
@@ -28,3 +30,20 @@ for nodeset in "${TESTNODESETS[@]}"; do
     python3 ../nodeset2owl.py ${nodeset}.xml -i ${BASE_ONTOLOGY} -v http://example.com/v0.1/UA/ -p test -o ${RESULT}
     diff ${nodeset}.ttl ${RESULT}
 done
+echo Starting testing specifications
+echo -------------------------------- 
+comparewith=core_cleaned.ttl
+echo Test ${CORE_NODESET}
+python3 ../nodeset2owl.py ${CORE_NODESET} -i ${BASE_ONTOLOGY} -v http://example.com/v0.1/UA/ -p opcua -o ${RESULT}
+python3 $CLEANGRAPH $RESULT $CLEANED 
+diff $CLEANED $comparewith
+nodeset=$DI_NODESET
+comparewith=devices_cleaned.ttl
+echo Test $DI_NODESET
+python3 ../nodeset2owl.py ${CORE_NODESET} -i ${BASE_ONTOLOGY} -v http://example.com/v0.1/UA/ -p opcua -o core.ttl
+python3 ../nodeset2owl.py  ${DI_NODESET} -i ${BASE_ONTOLOGY} core.ttl -v http://example.com/v0.1/DI/ -p devices -o ${RESULT}
+python3 $CLEANGRAPH $RESULT $CLEANED 
+diff $CLEANED $comparewith
+rm -f core.ttl
+
+#rm -f ${CLEANED} ${RESULT}
